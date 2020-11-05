83d45cc8f01af31be53084b4806b5ab28ca1fd48
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-05 12:46:02 +0100

    Add Footer, Main and Navbar components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import MainPage from './components/Main';
 import Navbar from './components/Navbar';
-import SearchBarRapid from './components/search-rapid/search-bar-rapid';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <div className="container">
-        <SearchBarRapid />
       </div>
       <MainPage />
       <Footer />
