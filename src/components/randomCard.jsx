a725bfde2ddc28de00debd668d4510ac46b1e389
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-12 15:46:03 +0100

    create randomCard component

---
+++ src/components/randomCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+
+class RandomCard extends React.Component {
+  render() {
+    return 
+    <div></div>
+    ;
+  }
+}
+
+export default RandomCard;
