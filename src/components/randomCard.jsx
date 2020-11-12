62af137ff63e9d5f272d47487ad15de4b2aec6be
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-12 16:03:49 +0100

    create axios

--- src/components/randomCard.jsx
+++ src/components/randomCard.jsx
@@ -2,10 +2,23 @@ import React from "react";
 import axios from "axios";
 
 class RandomCard extends React.Component {
+constructor(props){
+super(props)
+
+}
+    componentDidMount(){
+    const url = 'https://api.magicthegathering.io/v1/cards';
+    axios.get(url)
+    .then(({data})=>{
+      console.log(data)  
+    })
+}
   render() {
-    return 
-    <div></div>
-    ;
+    return( 
+    <div>
+        <p>fffzf</p>
+    </div>
+    );
   }
 }
 
