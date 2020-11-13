edb04e20cceb3a963ebd0d8f74a01eccac3b4914
Amelie Macquet <amelie.macquet.am@gmail.com>
2020-11-13 12:11:26 +0100

    creation d'un select avec la liste des sets

--- src/components/Booster.jsx
+++ src/components/Booster.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
-function Booster() {
-    return(
-        <div>
-            <h1>Booster</h1>
-        </div>
-    )
+
+class Booster extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       sets: [],
+       name: "",
+       code:""
+       
+    };
+}
+componentDidMount(){
+  axios.get("https://api.magicthegathering.io/v1/sets").then(({data})=>{
+    console.log(data);
+    console.log(data.sets[0].name);
+    console.log(data.sets[0].code);
+    this.setState({ 
+      sets: data.sets
+      
+    
+    });
+  });
+  
+};
+
+
+
+  
+  render(){
+   
+    return (
+      <div className="App">
+        <h1>Liste des extensions pour booster</h1>
+        <div>  
+        <select value={this.state.value} > {this.state.sets.map(set => (   
+            <option value={set.code}>{set.name}</option>
+            ))}  
+          </select>
+          </div> 
+          
+        
+      </div>
+    );
+
+  }
+  
 }
 
-export default Booster
+export default Booster;
