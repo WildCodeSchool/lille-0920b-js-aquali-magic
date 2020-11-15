810a83593069058ac2effa470beb6e0436106c6d
Amelie Macquet <amelie.macquet.am@gmail.com>
2020-11-15 10:29:24 +0100

    ajout du onchange

--- src/components/Booster.jsx
+++ src/components/Booster.jsx
@@ -13,9 +13,12 @@ class Booster extends React.Component {
     };
 }
 
+  onChange = (e) => {this.setState({ code: e.target.value })};
+  onSubmit = (e) => {} 
 
-  
-async componentDidMount() {
+
+
+  async componentDidMount() {
     const res = await axios.get("https://api.magicthegathering.io/v1/sets");
     this.setState({ sets: res.data.sets });
     console.log(this.state.sets);
@@ -26,15 +29,19 @@ async componentDidMount() {
   render(){
    
     return (
-      <div className="Booster">
-        <h1>Liste des extensions pour booster</h1>
-        <form>  
-        <select value={this.state.value}> {this.state.sets.map(set => (   
-            <option value={set.code}>{set.name}</option>
-            ))}  
-          </select>
-          <input type="submit" value="Booster" />
-          </form> 
+      <div className="Booster"> 
+
+        <h1>List of sets for booster</h1>
+
+        <form onSubmit={this.onSubmit}>  
+           <select value={this.state.value}>onChange={this.OnChange}
+              <option selected value="booster">Select a set to generate booster</option> 
+              {this.state.sets.map(set => (   
+              <option key={set.id} value={set.code}>{set.name}</option>
+              ))}  
+           </select>
+           <input type="submit" value="BOOSTER" />
+        </form> 
           
         
       </div>
