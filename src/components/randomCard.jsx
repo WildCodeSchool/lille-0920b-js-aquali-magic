f170d37765e7015d6ddc7b9433b80eea0e2a32bd
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-16 21:55:05 +0100

    Adding styled components button and card section

--- src/components/randomCard.jsx
+++ src/components/randomCard.jsx
@@ -1,5 +1,47 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const Div = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+const CardSection = styled.div`
+display : flex;
+align-items:center;
+margin-top: 24vh
+`
+
+const Button = styled.button`
+display : flex;
+align-items:center;
+margin-top: 40vh;
+padding: 14px 40px;
+border-radius: 7px;
+background-color: black;
+color : orange;
+font-weight : bold;
+font-size: 3vh;
+transition-duration: 0.4s;
+&:hover {
+  background-color: #e04b10;
+  color: white;
+}
+
+`
+
+const P = styled.p`
+color: white;
+font-weight : bold;
+padding-left : 2vw;
+font-size: 3vh;
+`
+
+const Img = styled.img`
+height:40vh;
+width:15vw;
+`
 
 class RandomCard extends React.Component {
   constructor(props) {
@@ -23,22 +65,30 @@ class RandomCard extends React.Component {
           cardImg: data.cards[0].imageUrl,
           cardAuthor: data.cards[0].artist,
         });
-      });   
-    };
-
+      });
+  };
 
   render() {
     return (
-      <div>
-        <section>
-          <p>{this.state.cardName}</p>
-          <p>{this.state.cardAuthor}</p>
-        </section>
-        <img src={this.state.cardImg} alt="Carte Indisponible" />
-        <button type="button" onClick={this.card}>
-          Découvrir une carte
-        </button>
-      </div>
+      <>
+        <Div>
+         
+          <section>
+            <Button type="button" onClick={this.card}>
+              Découvrir une carte
+            </Button>
+          </section>
+         <CardSection>
+           <Img src={this.state.cardImg} alt="Carte Indisponible" />
+           <section>
+            <P>Nom : {this.state.cardName}</P>
+            <P>Artiste :{this.state.cardAuthor}</P>
+            </section>
+            
+          </CardSection>
+        
+        </Div>
+      </>
     );
   }
 }
