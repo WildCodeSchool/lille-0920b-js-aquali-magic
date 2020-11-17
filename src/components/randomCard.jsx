2e88fef5c84d4ef842ea82caa96d97cc656c3efd
Gauthier-Lhomme <lhomme.gaut@gmail.com>
2020-11-17 15:25:28 +0100

    add text and css

--- src/components/randomCard.jsx
+++ src/components/randomCard.jsx
@@ -1,48 +1,66 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { device } from "./Device.jsx";
 
 const Div = styled.div`
   display: flex;
   justify-content: space-around;
+  @media ${device.mobile} {
+    display: none;
+  }
 `;
 
 const CardSection = styled.div`
-display : flex;
-align-items:center;
-margin-top: 24vh
-`
+  display: flex;
+  align-items: center;
+  margin-top: 24vh;
+`;
 
 const Button = styled.button`
-display : flex;
-align-items:center;
-margin-top: 40vh;
-padding: 14px 40px;
-border-radius: 7px;
-background-color: black;
-color : orange;
-font-weight : bold;
-font-size: 3vh;
-transition-duration: 0.4s;
-&:hover {
-  background-color: #e04b10;
-  color: white;
-}
-
-`
+  display: flex;
+  align-items: center;
+  margin-top: 10vh;
+  margin-left: 19vh;
+  padding: 14px 40px;
+  border-radius: 7px;
+  background-color: black;
+  color: orange;
+  font-weight: bold;
+  font-size: 3vh;
+  transition-duration: 0.4s;
+  &:hover {
+    background-color: #e04b10;
+    color: white;
+  }
+`;
 
 const P = styled.p`
-color: white;
-font-weight : bold;
-padding-left : 2vw;
-font-size: 3vh;
-`
+  color: white;
+  font-weight: bold;
+  padding-left: 2vw;
+  font-size: 3vh;
+`;
+
+const Pbutton = styled.p`
+  color: white;
+  width: 40vw;
+  font-size: 3vh;
+  padding-top: 13vh;
+`;
 
 const Img = styled.img`
-height:40vh;
-width:15vw;
-`
-
+  height: 40vh;
+  width: 15vw;
+  margin: 4px;
+  transform: scale(1);
+  perspective: 600px;
+  transition: all 250ms ease-out;
+  &:hover {
+    z-index: 10;
+    transform: scale(1.3);
+  }
+`;
 class RandomCard extends React.Component {
   constructor(props) {
     super(props);
@@ -57,9 +75,6 @@ class RandomCard extends React.Component {
     axios
       .get("https://api.magicthegathering.io/v1/cards?pageSize=1&random=true")
       .then(({ data }) => {
-        console.log(data.cards[0].name);
-        console.log(data.cards[0].imageUrl);
-        console.log(data.cards[0].artist);
         this.setState({
           cardName: data.cards[0].name,
           cardImg: data.cards[0].imageUrl,
@@ -72,21 +87,27 @@ class RandomCard extends React.Component {
     return (
       <>
         <Div>
-         
           <section>
+            <Pbutton>
+              Pendant la majeure partie des vingt-six ans d'existence de Magic,
+              l'unité de base de Magic a été le booster. La grande majorité des
+              cartes Magic qui ont été ouvertes proviennent d'un booster.
+              Pendant des années, pour la plupart des joueurs, ouvrir des
+              boosters fut l'essence même des produits Magic. <br />
+              Redécouvrez les cartes qui ont ryhtmé les champs de batailles des
+              Planeswalkers.
+            </Pbutton>
             <Button type="button" onClick={this.card}>
               Découvrir une carte
             </Button>
           </section>
-         <CardSection>
-           <Img src={this.state.cardImg} alt="Carte Indisponible" />
-           <section>
-            <P>Nom : {this.state.cardName}</P>
-            <P>Artiste :{this.state.cardAuthor}</P>
+          <CardSection>
+            <Img src={this.state.cardImg} alt="" />
+            <section>
+              <P>Nom : {this.state.cardName}</P>
+              <P>Artiste :{this.state.cardAuthor}</P>
             </section>
-            
           </CardSection>
-        
         </Div>
       </>
     );
@@ -94,5 +115,3 @@ class RandomCard extends React.Component {
 }
 
 export default RandomCard;
-
-//4980 avec cards/id
