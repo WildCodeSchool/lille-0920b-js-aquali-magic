366d4407c3ddd7afa3c928bb2f6ac12b84ef670f
AmjadWebDev <amjadwebdev@gmail.com>
2020-11-24 23:02:37 +0100

    add url to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 
- const Slidebar=(props) => {
+const Slidebar = (props) => {
   return (
     <Menu>
-      <a className="menu-item" href="/">
+      <a className="menu-item" href="/histoire">
         Histoire
       </a>
       <a className="menu-item" href="/">
         Règles
       </a>
-      <a className="menu-item" href="/">
+      <a className="menu-item" href="/carte">
         Cartes
       </a>
       <a className="menu-item" href="/">
         Créer
       </a>
-      <a className="menu-item" href="/">
+      <a className="menu-item" href="/booster">
         Booster
       </a>
     </Menu>
