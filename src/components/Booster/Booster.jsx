776550e15a08d5dbc42fb604724e6698c1f90026
Amelie Macquet <amelie.macquet.am@gmail.com>
2020-11-25 09:14:21 +0100

    modification du code

--- src/components/Booster/Booster.jsx
+++ src/components/Booster/Booster.jsx
@@ -8,9 +8,9 @@ import {
   BgImage,
   CallApi,
   SetForm,
-  Select, 
+  Select,
   Input,
-  ButtonClear
+  ButtonClear,
 } from "./Boosterstyled";
 
 class Booster extends React.Component {
@@ -35,20 +35,17 @@ class Booster extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.getBosster();
-    
   };
   handleReset = (e) => {
     e.preventDefault();
     this.setState({
-      cards: []
+      cards: [],
     });
-  }
+  };
 
   async componentDidMount() {
-    
     const res = await axios.get("https://api.magicthegathering.io/v1/sets");
     this.setState({ sets: res.data.sets });
-   
   }
 
   getBosster = async () => {
@@ -56,35 +53,34 @@ class Booster extends React.Component {
     const res = await axios.get(
       `https://api.magicthegathering.io/v1/sets/${this.state.code}/booster`
     );
-    this.setState({ cards: res.data.cards, loading:false });
-    console.log(this.state.cards);
+    this.setState({ cards: res.data.cards, loading: false });
+    
   };
 
   render() {
-    const { sets, code, cards , loading} = this.state;
+    const { sets, code, cards, loading } = this.state;
     return (
       <BgImage>
         <CallApi>
           <Title>List of sets for booster</Title>
           <SetForm onSubmit={this.handleSubmit}>
-          <Select value={code} onChange={this.handleChange}>
-            <option selected disabled hidden value="">
-              Select a set to generate booster
-            </option>
-            {sets.map((set) => {
-              {
-                if (!set.booster) {
-                  return null;
+            <Select value={code} onChange={this.handleChange}>
+              <option selected disabled hidden value="">
+                Select a set to generate booster
+              </option>
+              {sets.map((set) => {
+                {
+                  if (!set.booster) {
+                    return null;
+                  }
                 }
-              }
-              return (
-                <option key={set.id} value={set.code}>
-                  {set.name}
-                </option>
-              );
-               
+                return (
+                  <option key={set.id} value={set.code}>
+                    {set.name}
+                  </option>
+                );
               })}
-          </Select>
+            </Select>
             <Input type="submit" value="BOOSTER" />
             <ButtonClear onClick={this.handleReset}>Clear</ButtonClear>
           </SetForm>
@@ -92,30 +88,29 @@ class Booster extends React.Component {
         {loading ? (
           <Spinner />
         ) : (
-        <Gallery>
-          {cards.map((card) => {
-            return (
-              <div>
-              <ReactCardFlip
-                isFlipped={this.state.isFlipped}
-                flipDirection="vertical"
-              >
-                <img
-                  src="https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg"
-                  alt=""
-                  onClick={this.handleClick}
-                />
-                <img src={card.imageUrl} alt="" 
-                onClick={this.handleClick}/>
-
-              </ReactCardFlip>
-              
-              </div>
-            );
-          })}
-          
-          
-        </Gallery>
+          <Gallery>
+            {cards.map((card) => {
+              return (
+                <div>
+                  <ReactCardFlip
+                    isFlipped={this.state.isFlipped}
+                    flipDirection="vertical"
+                  >
+                    <img
+                      src="https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg"
+                      alt=""
+                      onClick={this.handleClick}
+                    />
+                    <img
+                      src={card.imageUrl}
+                      alt=""
+                      onClick={this.handleClick}
+                    />
+                  </ReactCardFlip>
+                </div>
+              );
+            })}
+          </Gallery>
         )}
       </BgImage>
     );
